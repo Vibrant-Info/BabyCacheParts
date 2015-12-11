4e41fc53d63f1680360816fe7daa4707418e13e1
vijaykamalr <vijaykamalr@gmail.com>
2015-12-11 11:05:08 +0530

    Storelist Colllapse added

--- public/controllers/storelistCtrl.js
+++ public/controllers/storelistCtrl.js
@@ -1,11 +1,11 @@
 app.controller('storelistCtrl',['$scope','$http',function($scope,$http){
 	
-	$scope.getValues=function(){
-		
+	var getValues=function(){
 	$http.get('/storelist').success(function(data){
 		$scope.values=data;
 	});
 	}
+	getValues();
 	$scope.checkActive=function(st,id){
 		$http.post('/storestatus',{st,id}).success(function(data){
 		console.log(data);
